c34b99adac97f0345d23fa60020defd38a709d10
Адмін <@DESKTOP-RQHPOUK>
2025-12-30 03:17:05 +0200

    Add project files.

---
+++ lab9/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace lab9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.btnBuild = new System.Windows.Forms.Button();
+            this.btnAddTrailer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(382, 549);
+            this.treeView1.TabIndex = 0;
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblPrice.Location = new System.Drawing.Point(399, 36);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(159, 25);
+            this.lblPrice.TabIndex = 1;
+            this.lblPrice.Text = "Total Price: 0 $";
+            // 
+            // btnBuild
+            // 
+            this.btnBuild.Location = new System.Drawing.Point(404, 104);
+            this.btnBuild.Name = "btnBuild";
+            this.btnBuild.Size = new System.Drawing.Size(154, 29);
+            this.btnBuild.TabIndex = 2;
+            this.btnBuild.Text = "Build Standard Truck";
+            this.btnBuild.UseVisualStyleBackColor = true;
+            // 
+            // btnAddTrailer
+            // 
+            this.btnAddTrailer.Location = new System.Drawing.Point(404, 158);
+            this.btnAddTrailer.Name = "btnAddTrailer";
+            this.btnAddTrailer.Size = new System.Drawing.Size(154, 27);
+            this.btnAddTrailer.TabIndex = 3;
+            this.btnAddTrailer.Text = "Add Trailer (+Parts)";
+            this.btnAddTrailer.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1400, 549);
+            this.Controls.Add(this.btnAddTrailer);
+            this.Controls.Add(this.btnBuild);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.Button btnBuild;
+        private System.Windows.Forms.Button btnAddTrailer;
+    }
+}
+
---
+++ lab9/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab9
+{
+    public partial class Form1 : Form
+    {
+        private CompositeUnit _truck;
+
+        public Form1()
+        {
+            InitializeComponent();
+            AttachEvents();
+        }
+
+        private void AttachEvents()
+        {
+            btnBuild.Click += (s, e) => BuildTruck();
+            btnAddTrailer.Click += (s, e) => AddTrailer();
+        }
+
+        private void BuildTruck()
+        {
+            _truck = new CompositeUnit("MAN Truck (Base)");
+
+            CompositeUnit chassis = new CompositeUnit("Chassis");
+            chassis.Add(new TruckPart("Axle Front", 500));
+            chassis.Add(new TruckPart("Axle Rear", 500));
+            chassis.Add(new TruckPart("Wheel FL", 200));
+            chassis.Add(new TruckPart("Wheel FR", 200));
+            chassis.Add(new TruckPart("Wheel RL", 200));
+            chassis.Add(new TruckPart("Wheel RR", 200));
+
+            CompositeUnit engine = new CompositeUnit("Engine V8");
+            engine.Add(new TruckPart("Block", 2000));
+            engine.Add(new TruckPart("Piston x8", 800));
+            engine.Add(new TruckPart("Turbo", 1500));
+
+            CompositeUnit cabin = new CompositeUnit("Cabin");
+            cabin.Add(new TruckPart("Seat Driver", 300));
+            cabin.Add(new TruckPart("Seat Passenger", 250));
+            cabin.Add(new TruckPart("Dashboard", 600));
+
+            _truck.Add(chassis);
+            _truck.Add(engine);
+            _truck.Add(cabin);
+
+            UpdateView();
+        }
+
+        private void AddTrailer()
+        {
+            if (_truck == null)
+            {
+                MessageBox.Show("Build the truck first!");
+                return;
+            }
+
+            CompositeUnit trailer = new CompositeUnit("Big Trailer");
+            trailer.Add(new TruckPart("Trailer Frame", 1000));
+            trailer.Add(new TruckPart("Extra Wheel 1", 200));
+            trailer.Add(new TruckPart("Extra Wheel 2", 200));
+            trailer.Add(new TruckPart("Container", 3000));
+
+            _truck.Add(trailer);
+            UpdateView();
+        }
+
+        private void UpdateView()
+        {
+            treeView1.Nodes.Clear();
+
+            TreeNode rootNode = new TreeNode($"{_truck.Name} - {_truck.GetPrice()}$");
+            treeView1.Nodes.Add(rootNode);
+
+            PrintNodesRecursive(rootNode, _truck);
+
+            treeView1.ExpandAll();
+            lblPrice.Text = $"Total Price: {_truck.GetPrice()} $";
+        }
+
+        private void PrintNodesRecursive(TreeNode treeNode, CompositeUnit composite)
+        {
+            foreach (var component in composite.GetChildren())
+            {
+                TreeNode newNode = new TreeNode($"{component.Name} - {component.GetPrice()}$");
+                treeNode.Nodes.Add(newNode);
+
+                if (component is CompositeUnit childComposite)
+                {
+                    PrintNodesRecursive(newNode, childComposite);
+                }
+            }
+        }
+    }
+
+    public abstract class TruckComponent
+    {
+        public string Name { get; protected set; }
+        public abstract int GetPrice();
+    }
+
+    public class TruckPart : TruckComponent
+    {
+        private int _price;
+
+        public TruckPart(string name, int price)
+        {
+            Name = name;
+            _price = price;
+        }
+
+        public override int GetPrice()
+        {
+            return _price;
+        }
+    }
+
+    public class CompositeUnit : TruckComponent
+    {
+        private List<TruckComponent> _children = new List<TruckComponent>();
+
+        public CompositeUnit(string name)
+        {
+            Name = name;
+        }
+
+        public void Add(TruckComponent component)
+        {
+            _children.Add(component);
+        }
+
+        public void Remove(TruckComponent component)
+        {
+            _children.Remove(component);
+        }
+
+        public override int GetPrice()
+        {
+            int total = 0;
+            foreach (var child in _children)
+            {
+                total += child.GetPrice();
+            }
+            return total;
+        }
+
+        public List<TruckComponent> GetChildren()
+        {
+            return _children;
+        }
+    }
+}
